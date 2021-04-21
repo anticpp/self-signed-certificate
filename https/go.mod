ce55110aa7de14848776550814bf8e6e23438635
supergui <supergui@live.cn>
2021-04-21 11:20:22 +0800

    https support insecure mode

--- https/go.mod
+++ https/go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.15
 
-require golang.org/x/net v0.0.0-20210420210106-798c2154c571 // indirect
+require golang.org/x/net v0.0.0-20210420210106-798c2154c571
--- https/main.go
+++ https/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
@@ -11,31 +12,38 @@ import (
 )
 
 func main() {
-	addr := flag.String("addr", ":4001", "HTTPS address")
-	//certFile := flag.String("certfile", "./cert.pem", "Certificate PEM file")
-	//keyFile := flag.String("keyfile", "./key.pem", "Key PEM file")
+	addr := flag.String("addr", ":4001", "Server address")
+	insecure := flag.Bool("insecure", false, "Insecure connection")
+	certFile := flag.String("certfile", "./cert.pem", "Certificate PEM file")
+	keyFile := flag.String("keyfile", "./key.pem", "Key PEM file")
 	flag.Parse()
 
-	mux := http.NewServeMux()
+	var mux = http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Printf("Handle http request, URI: %v\n", req.URL.RequestURI())
 		fmt.Fprintf(w, "Prouldly served with Go and HTTPS!")
 	})
 
-	h2s := &http2.Server{}
 	srv := &http.Server{
-		Addr: *addr,
-		//Handler: mux,
-		Handler: h2c.NewHandler(mux, h2s),
-		/*TLSConfig: &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			PreferServerCipherSuites: true,
-		},*/
+		Addr:    *addr,
+		Handler: h2c.NewHandler(mux, &http2.Server{}),
 	}
 
-	log.Printf("Starting server on %v\n", *addr)
-	//err := srv.ListenAndServeTLS(*certFile, *keyFile)
-	err := srv.ListenAndServe()
+	if *insecure == false {
+		srv.TLSConfig = &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			PreferServerCipherSuites: true,
+		}
+	}
+
+	var err error
+	if *insecure == false {
+		log.Printf("Starting server on https://localhost%v\n", *addr)
+		err = srv.ListenAndServeTLS(*certFile, *keyFile)
+	} else {
+		log.Printf("Starting server on http://localhost%v\n", *addr)
+		err = srv.ListenAndServe()
+	}
 	if err != nil {
 		log.Fatalf("Fail to start https server: %v", err)
 	}
