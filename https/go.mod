2eb46b6d78e887b72cb585b1f62ca9b9be39886f
supergui <supergui@live.cn>
2021-04-21 10:51:25 +0800

    h2 with unencrypted

--- https/go.mod
+++ https/go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.15
+
+require golang.org/x/net v0.0.0-20210420210106-798c2154c571 // indirect
--- https/main.go
+++ https/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
 	addr := flag.String("addr", ":4001", "HTTPS address")
-	certFile := flag.String("certfile", "./cert.pem", "Certificate PEM file")
-	keyFile := flag.String("keyfile", "./key.pem", "Key PEM file")
+	//certFile := flag.String("certfile", "./cert.pem", "Certificate PEM file")
+	//keyFile := flag.String("keyfile", "./key.pem", "Key PEM file")
 	flag.Parse()
 
 	mux := http.NewServeMux()
@@ -20,17 +22,20 @@ func main() {
 		fmt.Fprintf(w, "Prouldly served with Go and HTTPS!")
 	})
 
+	h2s := &http2.Server{}
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: mux,
-		TLSConfig: &tls.Config{
+		Addr: *addr,
+		//Handler: mux,
+		Handler: h2c.NewHandler(mux, h2s),
+		/*TLSConfig: &tls.Config{
 			MinVersion:               tls.VersionTLS12,
 			PreferServerCipherSuites: true,
-		},
+		},*/
 	}
 
 	log.Printf("Starting server on %v\n", *addr)
-	err := srv.ListenAndServeTLS(*certFile, *keyFile)
+	//err := srv.ListenAndServeTLS(*certFile, *keyFile)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Fail to start https server: %v", err)
 	}
